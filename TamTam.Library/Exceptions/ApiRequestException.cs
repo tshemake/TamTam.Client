3a3f14ba267faec07c4819d1003d4194caa2d976
@tshemake <tshemake@gmail.com>
2019-05-21 20:02:57 +0300

    Added ApiRequestException

---
+++ TamTam.Library/Exceptions/ApiRequestException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TamTam.Bot.Exceptions
+{
+    public class ApiRequestException : Exception
+    {
+        public virtual int ErrorCode { get; }
+
+        public ApiRequestException(string message, int errorCode, Exception innerException)
+            : base(message, innerException)
+        {
+            ErrorCode = errorCode;
+        }
+    }
+}
--- TamTam.Library/Sender/SenderApi.cs
+++ TamTam.Library/Sender/SenderApi.cs
@@ -9,6 +9,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using TamTam.Bot.Connector;
+using TamTam.Bot.Exceptions;
 using TamTam.Bot.Schema;
 
 namespace TamTam.Bot.Sender
@@ -111,64 +112,81 @@ namespace TamTam.Bot.Sender
                 var byteArrayContent = new ByteArrayContent(streamContent.ReadAsByteArrayAsync().Result);
                 byteArrayContent.Headers.ContentType = MediaTypeHeaderValue.Parse("multipart/form-data");
                 content.Add(byteArrayContent, "data", assetName);
-                using (var httpResponse = await connectorClient.HttpClient.PostAsync(requestUri, content))
+                using (var req = new HttpRequestMessage(HttpMethod.Post, requestUri))
                 {
-                    return await OnResponse<T>(httpResponse);
+                    if (content != null)
+                    {
+                        req.Content = content;
+                    }
+                    return await SendAsync<T>(connectorClient, req, cancellationToken);
                 }
             }
         }
 
         public static async Task<ApiResponse<T>> SendAsync<T>(IConnectorClient connectorClient, IRequest<T> request, CancellationToken cancellationToken = default)
         {
+            using (var req = new HttpRequestMessage(request.HttpMethod, request.Uri))
+            {
+                if (request.Payload != null)
+                {
+                    string requestCmd = JsonConvert.SerializeObject(request.Payload, SerializationSettings);
+                    req.Content = new StringContent(requestCmd, Encoding.UTF8, ContentTypes.ApplicationJson);
+                }
+                return await SendAsync<T>(connectorClient, req, cancellationToken);
+            }
+        }
+
+        public static async Task<ApiResponse<T>> SendAsync<T>(IConnectorClient connectorClient, HttpRequestMessage request, CancellationToken cancellationToken = default)
+        {
+            using (var httpResponse = await SendAsyncCancellationSafeAsync(connectorClient.HttpClient, request, cancellationToken))
+            {
+                if (IsJsonContentType(httpResponse))
+                {
+                    return await OnResponse<T>(httpResponse);
+                }
+                return new ApiResponse<T>(false, default, new ResultInfo(httpResponse.StatusCode, $"Service sent unknown content-type from url {request.RequestUri}."));
+            }
+        }
+
+        public static async Task<HttpResponseMessage> SendAsyncCancellationSafeAsync(HttpClient instance, HttpRequestMessage request, CancellationToken cancellationToken)
+        {
             try
             {
-                using (var req = new HttpRequestMessage(request.HttpMethod, request.Uri))
-                {
-                    if (request.Payload != null)
-                    {
-                        string requestCmd = JsonConvert.SerializeObject(request.Payload, SerializationSettings);
-                        req.Content = new StringContent(requestCmd, Encoding.UTF8, ContentTypes.ApplicationJson);
-                    }
-                    using (var httpResponse = await connectorClient.HttpClient.SendAsync(req, cancellationToken))
-                    {
-                        if (IsJsonContentType(httpResponse))
-                        {
-                            return await OnResponse<T>(httpResponse);
-                        }
-                        return new ApiResponse<T>(false, default, new ResultInfo(httpResponse.StatusCode, $"Service sent unknown content-type from url {request.Uri}."));
-                    }
-                }
+                return await instance.SendAsync(request, cancellationToken);
             }
-            catch (Exception ex)
+            catch (TaskCanceledException ex)
             {
-                return new ApiResponse<T>(false, default, new ResultInfo(ex));
+                cancellationToken.ThrowIfCancellationRequested();
+
+                throw new ApiRequestException("Failed to perform HTTP request", (int)HttpStatusCode.RequestTimeout, ex);
             }
         }
 
         private static async Task<ApiResponse<T>> OnResponse<T>(HttpResponseMessage httpResponse)
         {
             var actualResponseStatusCode = httpResponse.StatusCode;
+            string responseJson = await httpResponse.Content.ReadAsStringAsync();
             switch (actualResponseStatusCode)
             {
-                case HttpStatusCode.OK:
+                case HttpStatusCode.OK when !string.IsNullOrWhiteSpace(responseJson):
                     {
-                        var value = await ReadContentAsJsonAsync<T>(httpResponse);
+                        var value = MapFromJson<T>(responseJson);
                         return new ApiResponse<T>(true, value, new ResultInfo(httpResponse.StatusCode));
                     }
-                case HttpStatusCode.BadRequest:
-                case HttpStatusCode.Unauthorized:
-                case HttpStatusCode.NotFound:
-                case HttpStatusCode.MethodNotAllowed:
-                case HttpStatusCode.TooManyRequests:
-                case HttpStatusCode.ServiceUnavailable:
+                case HttpStatusCode.BadRequest when !string.IsNullOrWhiteSpace(responseJson):
+                case HttpStatusCode.Unauthorized when !string.IsNullOrWhiteSpace(responseJson):
+                case HttpStatusCode.NotFound when !string.IsNullOrWhiteSpace(responseJson):
+                case HttpStatusCode.MethodNotAllowed when !string.IsNullOrWhiteSpace(responseJson):
+                case HttpStatusCode.TooManyRequests when !string.IsNullOrWhiteSpace(responseJson):
+                case HttpStatusCode.ServiceUnavailable when !string.IsNullOrWhiteSpace(responseJson):
                     {
-                        var value = await ReadContentAsJsonAsync<Error>(httpResponse);
+                        var value = MapFromJson<Error>(responseJson);
                         return new ApiResponse<T>(false, default, new ResultInfo(httpResponse.StatusCode, value.Code, value.Message));
                     }
                 default:
                     {
                         httpResponse.EnsureSuccessStatusCode();
-                        return null;
+                        return new ApiResponse<T>(true, default, new ResultInfo(httpResponse.StatusCode));
                     }
             }
         }
@@ -176,7 +194,12 @@ namespace TamTam.Bot.Sender
         private static async Task<TResult> ReadContentAsJsonAsync<TResult>(HttpResponseMessage message)
         {
             var content = await message.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<TResult>(content, DeserializationSettings);
+            return MapFromJson<TResult>(content);
+        }
+
+        private static TResult MapFromJson<TResult>(string json)
+        {
+            return JsonConvert.DeserializeObject<TResult>(json, DeserializationSettings);
         }
 
         private static bool IsJsonContentType(HttpResponseMessage httpResponse)
